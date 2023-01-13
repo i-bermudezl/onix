4cdf883f4f10be6313d77efa8529608dfcd087c8
orion160 <idbl64@outlook.com>
2023-01-13 14:26:38 -0500

    Sphere

--- onix/main.cpp
+++ onix/main.cpp
@@ -5,6 +5,8 @@
 #include <numbers>
 #include <vector>
 
+#include <random>
+
 #include <Eigen/Dense>
 #include <OpenEXR/ImfRgbaFile.h>
 
@@ -35,11 +37,16 @@ void saveToFile(std::string name, const Buffer<Color> &buffer)
 
 int main(int argc, char **argv)
 {
-    int width{1920};
-    int height{1080};
+    int width{9};
+    int height{9};
 
     double aspectRatio{static_cast<double>(width) / height};
     double fov{std::numbers::pi / 4};
+
+    constexpr int samplesPerPixel = 100;
+
+    std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    std::mt19937 generator;
 
     auto frameBuffer = Buffer<Color>{width, height};
 
@@ -52,56 +59,60 @@ int main(int argc, char **argv)
     {
         for (int j{0}; j < width; j++)
         {
-            // pixel space
-            auto pX{static_cast<double>(j)};
-            auto pY{static_cast<double>(i)};
-
-            // ndc
-            auto dX{(pX + 0.5) / width};
-            auto dY{(pY + 0.5) / height};
-
-            // screen space
-            auto sX{2 * dX - 1};
-            auto sY{1 - 2 * dY};
-
-            // camera space
-            auto cX{sX * aspectRatio * tan(fov / 2)};
-            auto cY{sY * tan(fov / 2)};
-
-            // generate ray
-            auto origin{Eigen::Vector3d{0.0, 0.0, 0.0}};
-            auto direction{Eigen::Vector3d{cX, cY, -1}};
-            direction.normalize();
-            Ray r{origin, direction};
-
-            // ray trace
-            HitRecord record{};
-            auto closestSoFar{std::numeric_limits<double>::infinity()};
-            bool hitAnything{false};
-
-            Color c{};
-
-            for (const auto &object : world)
+            Eigen::Vector3d color{0.0, 0.0, 0.0};
+            for (int k{0}; k < samplesPerPixel; k++)
             {
-                if (object->hit(r, 0.0, closestSoFar, record))
+                // pixel space
+                auto pX{static_cast<double>(j)};
+                auto pY{static_cast<double>(i)};
+
+                // ndc
+                auto dX{(pX + 0.5) / width};
+                auto dY{(pY + 0.5) / height};
+
+                // screen space
+                auto sX{2 * dX - 1};
+                auto sY{1 - 2 * dY};
+
+                // camera space
+                auto cX{sX * aspectRatio * tan(fov / 2)};
+                auto cY{sY * tan(fov / 2)};
+
+                // generate ray
+                auto origin{Eigen::Vector3d{0.0, 0.0, 0.0}};
+                auto direction{Eigen::Vector3d{cX, cY, -1}};
+                direction.normalize();
+                Ray r{origin, direction};
+
+                // ray trace
+                HitRecord record{};
+                auto closestSoFar{std::numeric_limits<double>::infinity()};
+                bool hitAnything{false};
+
+                Color c{};
+
+                for (const auto &object : world)
                 {
-                    hitAnything = true;
-                    closestSoFar = record.t;
+                    if (object->hit(r, 0.0, closestSoFar, record))
+                    {
+                        hitAnything = true;
+                        closestSoFar = record.t;
+                    }
                 }
-            }
 
-            // coloring
-            if (hitAnything)
-            {
-                Eigen::Vector3d vk = record.normal;
-                c = Color{0.5 * (vk.x() + 1.0), 0.5 * (vk.y() + 1.0), 0.5 * (vk.z() + 1.0), 1.0};
-            }
-            else
-            {
-                c = Color{1.0, 1.0, 1.0, 1.0};
-            }
+                // coloring
+                if (hitAnything)
+                {
+                    Eigen::Vector3d vk = record.normal;
+                    c = Color{0.5 * (vk.x() + 1.0), 0.5 * (vk.y() + 1.0), 0.5 * (vk.z() + 1.0), 1.0};
+                }
+                else
+                {
+                    c = Color{1.0, 1.0, 1.0, 1.0};
+                }
 
-            frameBuffer.write(c, j, i);
+                frameBuffer.write(c, j, i);
+            }
         }
     }
     auto er{std::chrono::steady_clock::now()};
